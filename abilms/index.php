9f99d083c801e38486111b738af33c09d3efcd77
merirose <45115683+merirose@users.noreply.github.com>
2019-02-03 05:51:51 +0800

    Add files via upload

---
+++ abilms/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Learn</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/custom.css">
+  <script src="js/jquery-1.12.1.min"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">Hello World</div>
+
+</body>
+</html>
---
+++ table_admin.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Abilms
+{
+    #region Table_admin
+    public class Table_admin
+    {
+        #region Member Variables
+        protected int _admin_id;
+        protected string _admin_name;
+        protected string _email;
+        protected string _password;
+        #endregion
+        #region Constructors
+        public Table_admin() { }
+        public Table_admin(string admin_name, string email, string password)
+        {
+            this._admin_name=admin_name;
+            this._email=email;
+            this._password=password;
+        }
+        #endregion
+        #region Public Properties
+        public virtual int Admin_id
+        {
+            get {return _admin_id;}
+            set {_admin_id=value;}
+        }
+        public virtual string Admin_name
+        {
+            get {return _admin_name;}
+            set {_admin_name=value;}
+        }
+        public virtual string Email
+        {
+            get {return _email;}
+            set {_email=value;}
+        }
+        public virtual string Password
+        {
+            get {return _password;}
+            set {_password=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
